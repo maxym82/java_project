76f7663ecc63e84c3f29f3d93718b4a999a5b9fe
maksym <maksym.iurchenko@gmail.com>
2019-07-08 16:09:02 -0700

    resources has been added, properties file added, static method to set up properties has been created

--- source/server/registry/src/main/java/registry/Application.java
+++ source/server/registry/src/main/java/registry/Application.java
@@ -3,10 +3,28 @@ package registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    public static void setSystemProperties() {
+        Properties prop = new Properties();
+        String propFile = "config.properties";
+        try {
+            InputStream readFile = Application.class.getClassLoader().getResourceAsStream(propFile);
+            prop.load(readFile);
+        } catch (Exception e) {
+            System.out.println("Can not read configuration file: \n" + e.getLocalizedMessage());
+        }
+        System.setProperty("dbURL", prop.getProperty("dbURL"));
+        System.setProperty("dbUserName", prop.getProperty("dbUserName"));
+        System.setProperty("dbPassword", prop.getProperty("dbPassword"));
     }
 }
