7365dccbce4a98283da1b5094e669d2883f79fbb
maksym <maksym.iurchenko@gmail.com>
2019-07-07 20:46:25 -0700

    singleton for connaction

--- source/server/registry/src/main/java/dataBase/DataBaseConnection.java
+++ source/server/registry/src/main/java/dataBase/DataBaseConnection.java
@@ -6,37 +6,31 @@ import java.sql.SQLException;
 
 public class DataBaseConnection {
 
-    private static DataBaseConnection instance;
-    private Connection connection = null;
-    private final String url;
-    private final String userName;
-    private final String password;
+    private static Connection connection = null;
+    private static final String url;
+    private static final String userName;
+    private static final String password;
 
     private DataBaseConnection () {
-        this.url = System.getProperty("dbURL");
-        this.userName = System.getProperty("dbUserName");
-        this.password = System.getProperty("dbPassword");
+    }
+    static {
+        url = System.getProperty("dbURL");
+        userName = System.getProperty("dbUserName");
+        password = System.getProperty("dbPassword");
     }
 
-    public void setConnection() {
+    public static Connection getConnection() {
         try {
-            this.connection = DriverManager.getConnection(url, userName, password);
-            System.out.println("Connection has been established");
+            if (connection == null) {
+                connection = DriverManager.getConnection(url, userName, password);
+                System.out.println("Connection has been established");
+            }else if (connection.isClosed()) {
+                connection = DriverManager.getConnection(url, userName, password);
+                System.out.println("Connection has been established");
+            }
         } catch (SQLException e) {
             System.out.println("Was not able to set up connection: \n" + e.getMessage());
         }
-    }
-
-    public Connection getConnection() {
         return connection;
-    }
-
-    public static DataBaseConnection getInstance() throws SQLException {
-        if (instance == null) {
-            instance = new DataBaseConnection();
-        } else if (instance.getConnection().isClosed()) {
-            instance = new DataBaseConnection();
-        }
-        return instance;
     }
 }
